4f5ccd8fc5b0d0db6e776f14d0cfca92c64ab130
Shiftyshadows <purple.quasar@proton.me>
2023-06-09 03:30:22 +0300

    5. Everything can be imported

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
-import importlib.util
-
 if __name__ == "__main__":
-    module_name = "variable_load_5"
-    module_path = "./variable_load_5.py"
-
-    spec = importlib.util.spec_from_file_location(module_name, module_path)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-
-    a = getattr(module, "a")
-    print(a)
+    from variable_load_5 import a
+print(a)
