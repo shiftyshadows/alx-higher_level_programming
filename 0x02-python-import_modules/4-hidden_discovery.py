2b6406ffda45c6f5f414f2cd45daa60ed8764405
Shiftyshadows <purple.quasar@proton.me>
2023-06-09 03:23:45 +0300

    4. Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import hidden_4
+
 if __name__ == "__main__":
     file = dir(hidden_4)
     fil_no = len(file)
-    for i in range(0, fil_no):
-        if file[i][0:2] != "__":
+    for i in range(fil_no):
+        if not file[i].startswith("__"):
             print(file[i])
