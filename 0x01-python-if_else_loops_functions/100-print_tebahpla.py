822cd7f71af7cef8fdc225d2c089e9aae45656e9
Shiftyshadows <purple.quasar@proton.me>
2023-06-29 09:54:40 +0300

    14. Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('A') - 1, -1):
-    print("{}{}".format(chr(i), chr(i - 32)) if i % 2 == 1 else '', end='')
+for i in range(122, 96, -1):
+    if i % 2 != 0:
+        print("{:c}".format(i - 32), end='')
+    else:
+        print("{:c}".format(i), end='')
