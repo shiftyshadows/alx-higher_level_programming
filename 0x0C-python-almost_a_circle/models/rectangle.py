3ca10a8fddef8a2c78a7c3ca49ae463c49362286
Shiftyshadows <purple.quasar@proton.me>
2023-07-17 14:51:23 +0300

    6. __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -64,3 +64,6 @@ class Rectangle(Base):
     def display(self):
         for _ in range(self.height):
             print("#" * self.width)
+
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
