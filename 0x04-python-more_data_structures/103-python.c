1baed93a61d8e29e662f9fd95be271417f4a3588
Shiftyshadows <purple.quasar@proton.me>
2023-07-19 20:59:13 +0300

    16. CPython #1: PyBytesObject

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,54 +1,53 @@
 #include <Python.h>
 #include <stdio.h>
 
-/**
- * Prints basic information about a Python list.
- *
- * @param p The Python list object.
- */
 void print_python_list(PyObject *p) {
     Py_ssize_t size, i;
+    PyObject *item;
 
-    size = PyObject_Length(p);
-
-    printf("[*] Python list info\n");
-    printf("[*] Size of the Python List = %zd\n", size);
-    printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
-
-    for (i = 0; i < size; i++) {
-        PyObject *item = PyList_GetItem(p, i);
-        const char *typeName = item->ob_type->tp_name;
-        printf("Element %zd: %s\n", i, typeName);
-    }
-}
-
-/**
- * Prints basic information about a Python bytes object.
- *
- * @param p The Python bytes object.
- */
-void print_python_bytes(PyObject *p) {
-    Py_ssize_t size, i;
-    char *str;
-
-    printf("[.] bytes object info\n");
-    if (!PyBytes_Check(p)) {
-        printf("  [ERROR] Invalid Bytes Object\n");
+    if (!PyObject_HasAttrString(p, "__iter__")) {
+        printf("Invalid Python list\n");
         return;
     }
 
     size = PyObject_Length(p);
-    str = ((PyBytesObject *)p)->ob_sval;
 
-    printf("  size: %zd\n", size);
+    printf("[*] Python list info\n");
+    printf("[*] Size of the list = %ld\n", size);
+
+    for (i = 0; i < size; i++) {
+        item = PySequence_GetItem(p, i);
+        printf("Element %ld: %s\n", i, item->ob_type->tp_name);
+        Py_DECREF(item);
+    }
+}
+
+void print_python_bytes(PyObject *p) {
+    Py_ssize_t size, i;
+    char *str;
+
+    if (!PyBytes_Check(p)) {
+        printf("Invalid Python bytes object\n");
+        return;
+    }
+
+    size = PyObject_Length(p);
+
+    printf("[.] bytes object info\n");
+    printf("  size: %ld\n", size);
+
+    if (size > 10)
+        size = 10;
+
+    str = PyUnicode_AsUTF8AndSize(p, NULL);
+
     printf("  trying string: %s\n", str);
 
-    printf("  first %zd bytes: ", size < 10 ? size : 10);
-    for (i = 0; i < size && i < 10; i++) {
+    printf("  first %ld bytes: ", size);
+    for (i = 0; i < size; i++) {
         printf("%02x", (unsigned char)str[i]);
-        if (i + 1 < size && i + 1 < 10) {
+        if (i < size - 1)
             printf(" ");
-        }
     }
     printf("\n");
 }
