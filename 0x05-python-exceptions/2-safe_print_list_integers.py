5646f936a8cd1f8fe1accc0e811d506d28168f53
Shiftyshadows <purple.quasar@proton.me>
2023-07-19 19:28:32 +0300

    2. Print and count integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        count = 0
+        for element in my_list:
+            if count >= x:
+                break
+            try:
+                print("{:d}".format(element), end=' ')
+                count += 1
+            except (TypeError, ValueError):
+                pass
+    except:
+        return 0
+    finally:
+        print()
+        return count
