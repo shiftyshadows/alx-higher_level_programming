548963fe10f5aa54750c8c0358450909f4d16c8f
Shiftyshadows <purple.quasar@proton.me>
2023-07-19 19:17:17 +0300

    0. Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
         count = 0
         for element in my_list:
             if count < x:
-                print(element)
+                print(element, end='')
                 count += 1
     except:
         pass
