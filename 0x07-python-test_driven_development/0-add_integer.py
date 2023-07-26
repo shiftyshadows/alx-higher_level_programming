8df03cd5d406871fab84252206bb7f1dbe746ff1
Shiftyshadows <purple.quasar@proton.me>
2023-07-26 16:06:36 +0300

    0. Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,6 +7,8 @@ The 0-add_integer module supplies one function, add_integer(a, b).
 
 def add_integer(a, b=98):
     """ Returns the result of addition of 2 integers"""
+    if len(locals()) > 2:
+        raise TypeError("add_integer() takes 2 positional arguments but more were given")
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
