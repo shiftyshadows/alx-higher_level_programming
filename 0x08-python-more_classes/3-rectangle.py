bb21f99e8bdb2c607fc763d0da619116ce129b86
Shiftyshadows <purple.quasar@proton.me>
2023-07-29 14:49:07 +0300

    3. String representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -53,6 +53,3 @@ class Rectangle:
             for _ in range(self._height):
                 rectangle_str += "#" * self._width + "\n"
             return rectangle_str[:-1]
-
-    def __repr__(self):
-        return f"Rectangle({self._width}, {self._height})"
