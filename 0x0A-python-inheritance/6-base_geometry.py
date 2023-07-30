b68eee415af1122e45e18b0298d8ca97f0563b11
Shiftyshadows <purple.quasar@proton.me>
2023-07-30 11:18:03 +0300

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module contains the BaseGeometry Class.
+"""
+
+
+class BaseGeometry:
+    """ A class with a public instance method"""
+    def area(self):
+        raise Exception("area() is not implemented")
