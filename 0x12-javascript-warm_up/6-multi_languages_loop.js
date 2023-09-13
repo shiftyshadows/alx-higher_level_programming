49543b42e2f4723eb87d58cdc94c0c95da603ad2
Shiftyshadows <purple.quasar@proton.me>
2023-09-13 14:03:43 +0300

    6. Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,2 +1,14 @@
 #!/usr/bin/node
+const lines = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
 
+let output = '';
+
+for (let i = 0; i < lines.length; i++) {
+  output += lines[i] + '\n';
+}
+
+console.log(output);
