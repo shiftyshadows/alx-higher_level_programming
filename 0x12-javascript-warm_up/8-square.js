a4dffbee310129a48cd38b513f571d7f5f9d4556
Shiftyshadows <purple.quasar@proton.me>
2023-09-13 14:36:58 +0300

    8. Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 const arg1 = parseInt(process.argv[2], 10);
 
-for (let i = 0; i < arg1; i++) {
-  const str = 'X'.repeat(arg1);
-  console.log(str);
+if (Number.isInteger(arg1)) {
+  for (let i = 0; i < arg1; i++) {
+    const str = 'X'.repeat(arg1);
+    console.log(str);
+  }
+} else {
+  console.log('Missing size');
 }
