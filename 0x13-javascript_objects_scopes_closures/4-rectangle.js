30637e6a963fb0d2442e550abc70538532fc8b0d
Shiftyshadows <purple.quasar@proton.me>
2023-09-24 22:05:11 +0300

    4. Rectangle #4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,31 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0 && typeof w === 'number' && typeof h === 'number') {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; ++i) {
+      let j = 0;
+      for (; j < this.width; ++j) {
+        process.stdout.write('X');
+      }
+      if (j === this.width) {
+        console.log('');
+      }
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
