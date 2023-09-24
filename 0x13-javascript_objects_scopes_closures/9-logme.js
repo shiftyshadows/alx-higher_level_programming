179ad1c4530fa3df1c2095954439f1409fb2c8d2
Shiftyshadows <purple.quasar@proton.me>
2023-09-24 22:51:15 +0300

    9. Log me

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+let count = 0; // Initialize a counter to keep track of the number of arguments printed
+
+exports.logMe = function (item) {
+  // Print the current argument value along with the count
+  console.log(`${count}: ${item}`);
+
+  // Increment the count for the next call
+  count++;
+};
