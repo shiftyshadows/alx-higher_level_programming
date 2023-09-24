292fa7a0b995615c000e25b24869afac86af410d
Shiftyshadows <purple.quasar@proton.me>
2023-09-24 22:30:43 +0300

    5. Square #0

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -3,11 +3,7 @@ const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
-    super(size, size);
-  }
-
-  double () {
-    super.double();
+      super(size, size)
   }
 }
 
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,26 @@
 #!/usr/bin/node
+const OldSquare = require('./5-square');
+
+class Square extends OldSquare {
+  constructor (size) {
+    super(size, size);
+  }
+
+  double () {
+    super.double();
+  }
+
+  charPrint (c) {
+    for (let i = 0; i < this.size; ++i) {
+      let j = 0;
+      for (; j < this.size; ++j) {
+        process.stdout.write(c);
+      }
+      if (j === this.size) {
+        console.log('');
+      }
+    }
+  }
+}
+
+module.exports = Square;
