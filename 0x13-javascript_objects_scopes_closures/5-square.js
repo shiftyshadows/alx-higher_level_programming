2350b472f3a3396b93f851d815d07fea44e8b8a6
Shiftyshadows <purple.quasar@proton.me>
2023-09-24 22:08:14 +0300

    5. Square #0

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  double () {
+    super.double();
+  }
+}
+
+module.exports = Square;
