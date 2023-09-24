042298bb6ae83b57cca73e881fae8d4cc8fba600
Shiftyshadows <purple.quasar@proton.me>
2023-09-24 22:47:20 +0300

    7. Occurrences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  // Initialize a count variable to keep track of the occurrences
+  let count = 0;
+
+  // Loop through the list and check each element
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      // If the current element matches the search element, increment the count
+      count++;
+    }
+  }
+
+  // Return the final count of occurrences
+  return count;
+};
