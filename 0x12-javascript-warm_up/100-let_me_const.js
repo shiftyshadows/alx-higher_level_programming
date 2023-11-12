d46946b624426279cacd9324906868e64c989a7c
Shiftyshadows <purple.quasar@proton.me>
2023-11-12 20:05:54 +0300

    12_nov

--- 0x12-javascript-warm_up/100-let_me_const.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/node
-myVar = 333;
-// Export the modified myVar so it can be accessed in other files
-module.exports = myVar;
--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const arg1 = process.argv[2];
-const parsedInt = parseInt(arg1);
-
-if (!isNaN(parsedInt)) {
-  console.log(`My number: ${parsedInt}`);
-} else {
-  console.log('Not a number');
-}
