0d9da873ae91ec9719588cb81715bc1db6917ff3
Shiftyshadows <purple.quasar@proton.me>
2024-01-04 04:16:00 +0300

    2. Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,20 +1,7 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-// Check if the correct number of arguments are provided
-if (process.argv.length !== 3) {
-  console.error('Usage: node getstatus.js <url>');
-  process.exit(1);
-}
-
-// Get the URL from command line arguments
-const url = process.argv[2];
-
-// Make a GET request and display the status code
-request.get(url, (error, response) => {
-  if (error) {
-    console.error('Error:', error);
-  } else {
-    console.log('Code:', response.statusCode);
-  }
+request(process.argv[2], function (_err, res) {
+  console.log('code:', res.statusCode); // Print the response status code if a response was received
 });
