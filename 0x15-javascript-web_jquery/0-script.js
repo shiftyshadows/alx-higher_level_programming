649389b047067135a78ec99c31e861f8594420e6
Shiftyshadows <purple.quasar@proton.me>
2024-01-16 02:21:59 +0300

    2. Click and turn red

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,8 @@
+// 0-script.js
+document.addEventListener('DOMContentLoaded', function () {
+  // Select the <header> element using document.querySelector
+  const headerElement = document.querySelector('header');
+
+  // Update the text color of the <header> element to red (#FF0000)
+  headerElement.style.color = '#FF0000';
+});
