75a1549c130d53fb85b458b269874746d1d50cde
Shiftyshadows <purple.quasar@proton.me>
2024-01-16 03:33:48 +0300

    7. Star wars character

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    $.ajax({
+        url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
+        method: 'GET',
+        success: function (data) {
+            $('#character').text(data.name);
+        },
+        error: function () {
+            $('#character').text('Error loading character');
+        }
+    });
+});
