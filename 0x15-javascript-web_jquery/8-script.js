df5a81a2f2c24cb73a11ded19842c4d939fc365d
Shiftyshadows <purple.quasar@proton.me>
2024-01-16 03:37:49 +0300

    11. List, add, remove

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
-    $.ajax({
-        url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
-        method: 'GET',
-        success: function (data) {
-            // Iterate through the results and append each title to the list
-            $.each(data.results, function (index, movie) {
-                $('#list_movies').append('<li>' + movie.title + '</li>');
-            });
-        },
-        error: function () {
-            $('#list_movies').append('<li>Error loading movies</li>');
-        }
-    });
+  $.ajax({
+    url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
+    method: 'GET',
+    success: function (data) {
+      // Iterate through the results and append each title to the list
+      $.each(data.results, function (index, movie) {
+        $('#list_movies').append('<li>' + movie.title + '</li>');
+      });
+    },
+    error: function () {
+      $('#list_movies').append('<li>Error loading movies</li>');
+    }
+  });
 });
